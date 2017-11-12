b7ae0c323022f386474000b19716e16dcc5203c3
Briant Oliveira <briantmoliveira@gmail.com>
2017-11-12 01:42:43 -0800

    created dictionary of words

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+import random
+import time
+
+'''This function will take a list and create random grouping of those words'''
+
+dictionary_of_words = {'happiness': 'good fortune; pleasure; contentment; joy.',
+                       'sad': 'affected by unhappiness or grief; sorrowful or mournful',
+                        'mournful': 'feeling or expressing sorrow or grief; sorrowful; sad'}
+
