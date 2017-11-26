a37a769785a2035deb63fde3ee71c041f5054f00
Briant Oliveira <briantmoliveira@gmail.com>
2017-11-26 09:09:40 -0800

    get function

--- hash_table.py
+++ hash_table.py
@@ -5,23 +5,32 @@ class HashMap:
         self.size = 6
         self.map = [None] * self.size
 
-    def _get_hash(self,key):
+def _get_hash(self,key):
 
-    def add(self,key, value):
-        key_hash = self._get_hash(key)
-        key_value = [key, value]
+    """ function check if key value is = to none if not add 1 """
+def add(self,key, value):
+    key_hash = self._get_hash(key)
+    key_value = [key, value]
 
-        if self.map[key_hash] is None:
-            self.map[key_hash] = list([key_value])
-            return True
-        else:
-            for pain in self.map[key_hash]:
-                if pain[0] == key
-                    pair[1] = value
-                    return True
-            self.map[key_hash].append[key_value]
+    if self.map[key_hash] is None:
+        self.map[key_hash] = list([key_value])
+        return True
+    else:
+        for pair in self.map[key_hash]:
+            if pair[0] == key():
+                pair[1] = value
+                return True
+        self.map[key_hash].append([key_value])
+    """ get the hash given the key and locate the cell and if cell is != none then loop and find/return value of the key"""
+def get(self,key):
+    key_hash = self._get_hash(key)
+    if self.map[key_hash] is not None:
+        for pair in self.map[key_hash]:
+            if pair[0] == key:
+                return pair[1]
 
-    def get(self,key):
+    def delete():
+
 
     def print(self):
 
