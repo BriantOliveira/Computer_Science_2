1b3e038996735c488d56b0991f7ddb56f178507a
Briant Oliveira <briantmoliveira@gmail.com>
2017-11-26 08:51:42 -0800

    functions

---
+++ hash_table.py
@@ -0,0 +1,22 @@
+
+
+class HashMap:
+    def __init__(self):
+        self.size = 6
+        self.map = [None] * self.size
+
+    def _get_hash(self,key):
+
+    def add(self,key, value):
+
+    def get(self,key):
+
+    def print(self):
+
+h = HashMap()
+h.add('Elliot', '978-677')
+h.add('Rohan', '774-312')
+h.add('Willie', '999-111')
+h.add('Alan', '678-234')
+h.add('Tassos', '911-569')
+h.add('Tia', '415-332')
