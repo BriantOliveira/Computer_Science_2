769bed43c2cd22e5d78ae7b3fe06d40099fdd6e4
Briant Oliveira <briantmoliveira@gmail.com>
2017-11-26 08:57:26 -0800

    add function

--- hash_table.py
+++ hash_table.py
@@ -8,6 +8,18 @@ class HashMap:
     def _get_hash(self,key):
 
     def add(self,key, value):
+        key_hash = self._get_hash(key)
+        key_value = [key, value]
+
+        if self.map[key_hash] is None:
+            self.map[key_hash] = list([key_value])
+            return True
+        else:
+            for pain in self.map[key_hash]:
+                if pain[0] == key
+                    pair[1] = value
+                    return True
+            self.map[key_hash].append[key_value]
 
     def get(self,key):
 
