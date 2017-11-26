ae8ed7732ac394e9a020293b67a02128d2d57abe
Briant Oliveira <briantmoliveira@gmail.com>
2017-11-26 10:15:33 -0800

    fixed bugs

--- hash_table.py
+++ hash_table.py
@@ -5,35 +5,39 @@ class HashMap:
         self.size = 6
         self.map = [None] * self.size
 
-def _get_hash(self,key):
-    hash = 0
-    for char in str(key):
-        hash += ord(char)
-    return hash % self.size
+
+    def _get_hash(self, key):
+        hash = 0
+        for char in str(key):
+            hash += ord(char)
+        return hash % self.size
 
     """ function check if key value is = to none if not add 1 """
-def _add_hash_(self,key, value):
-    key_hash = self._get_hash(key)
-    key_value = [key, value]
+    def _add_hash_(self, key, value):
+        key_hash = self._get_hash(key)
+        key_value = [key, value]
 
-    if self.map[key_hash] is None:
-        self.map[key_hash] = list([key_value])
-        return True
-    else:
-        for pair in self.map[key_hash]:
-            if pair[0] == key():
-                pair[1] = value
-                return True
-        self.map[key_hash].append([key_value])
+        if self.map[key_hash] is None:
+            self.map[key_hash] = list([key_value])
+            return True
+        else:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    pair[1] = value
+                    return True
+            self.map[key_hash].append(key_value)
+            return True
+
     """ get the hash given the key and locate the cell and if cell is != none then loop and find/return value of the key"""
-def _get_hash_(self,key):
-    key_hash = self._get_hash(key)
-    if self.map[key_hash] is not None:
-        for pair in self.map[key_hash]:
-            if pair[0] == key:
-                return pair[1]
+    def _get_(self, key):
+        key_hash = self._get_hash(key)
+        if self.map[key_hash] is not None:
+            for pair in self.map[key_hash]:
+                if pair[0] == key:
+                    return pair[1]
+
     """function that will locate the key and check if the cell is none means that is does not exist"""
-    def _delete_hash_():
+    def _delete_hash_(self, key):
         key_hash = self._get_hash(key)
 
         if self.map[key_hash] is None:
@@ -43,26 +47,28 @@ def _get_hash_(self,key):
                 self.map[key_hash].pop(i)
                 return True
 
-    def _display_hash_(self):
+    def _print_(self):
         print("---PHONEBOOK---")
         for item in self.map:
             if item is not None:
                 print(str(item))
 
 
-h = HashMap()
+my_hash = HashMap()
 
-h.add('Elliot', '978-677')
-h.add('Rohan', '774-312')
-h.add('Willie', '999-111')
-h.add('Alan', '678-234')
-h.add('Tassos', '911-569')
-h.add('Tia', '415-332')
+my_hash._add_hash_('Elliot', '978-6770')
+my_hash._add_hash_('Rohan', '774-3120')
+my_hash._add_hash_('Rohan', '562-7890')
+my_hash._add_hash_('Willie', '999-1110')
+my_hash._add_hash_('Alan', '774-2340')
+my_hash._add_hash_('Tassos', '911-5690')
+my_hash._add_hash_('Tia', '978-3320')
+my_hash._add_hash_('Alan', '305-8080')
 
-h.display()
+my_hash._print_()
 
-h.delete('Elliot')
+my_hash._delete_hash_('Elliot')
 
-h.display()
+my_hash._print_()
 
-print('Rohan:' + h.get('Rohan'))
+print('Rohan: ' + my_hash._get_('Rohan'))
