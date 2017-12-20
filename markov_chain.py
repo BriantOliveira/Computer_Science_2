f84aa084d4b4d5946a13f5d79c846fe19d191381
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-20 18:21:54 -0500

    trying to fix bug

--- markov_chain.py
+++ markov_chain.py
@@ -4,105 +4,120 @@ from random import random as ri
 import sys
 
 class Markov_chain_Nth_order(object):
+            def __init__(self, order=1):
+                self.states = {}
 
-
-    def __init__(self, order=1):
-        self.states = {}
-
-        if order > 0:
-            self.order = order
-        else:
-            raise Expection("\nInvalid parameter: Integer only.")
-
-
-    def initialize_chain(self):
-        n = self.order
-        chain = []
-
-        #tuple with N size
-        for _ in range(n):
-            chain.append("")
-
-        return tuple(chain)
-
-
-    def add_to_chain(self, old, step):
-        new = old[1:] + (step, )
-        return new
-
-
-    def create_states(self, tokens):
-        """Creating states from the sentences"""
-        prev = self.initialize_chain()
-
-        #Create Markov
-        for token in tokens:
-            if not prev in self.states:
-                self.states[prev] = []
-
-                cur = self.add_to_chain(prev, token)
-                self.states[prev].append(cur)
-
-                prev = cur
-        input_sentence = " ".join(tokens)
-        print("\nInput Sentece: {}... \n".format(input_sentence[:400]))
-        print("WORD COUNT OF CORPUS: >400,000\n")
-
-        return
-
-
-    def construct_sentence(self):
-        """Sample Dictagram and build new sentences"""
-
-    chain = self.initialize_chain()
-    cur_position = self.select_cur_position(chain)
-    start, delimiter, is_first = "", " ", True
-
-    print("CHAIN: {}\n".format(chain))
-
-    while (cur_position in self.states) and (cur_position != chain) and (cur_position != None):
-        if not is_first:
-            start += delimiter
-
-        start += cur_position[len(cur_position) -1]
-        cur_position = self.select_cur_position(cur_position)
-        is_first = False
-
-    walk_over = start + delimiter + cur_position[len(cur_position) -1]
-
-    return walk_over
-
-
-    def select_cur_position(self, pos):
-        """Method to select word position in the corpus randomly"""
-        cur_position = self.states[pos][ri(0, len(self.state[pos]) -1)]
-        return cur_position
-
-
-    def create_markov_model():
-        """Create and run class instance, create copus from Harry Potter book"""
-
-        with open("harry_potter_books.txt") as f:
-            corpus = f.read().split()
-
-            if len(sys.argv) > 1:
-                if sys.argv[1].isnumeric():
-                    print("Numeric input received successfully... LODING... \n\nORDER OF MARKOV:\nn = {}".format(int(sys.argv[1])))
-                    markov = Markov_chain_Nth_order(int(sys.argv[1]))
-
+                if order > 0:
+                    self.order = order
                 else:
-                    print("ERROR INPUT INVALID. REVERTING TO DEFAULT:\n\nORDER OF MARKOV:\n\n = 1")
-                    markov = Markov_chain_Nth_order()
+                    raise Expection("\nInvalid parameter: Integer only.")
 
-            else:
-                print("DID NOT RECEIVED AN INPUT... REVERTING TO DEFAULT:\n\nORDER OF MARKOV:\nn =1")
-                markov = Markov_chain_Nth_order()
 
-                # Random walk over then reformating to final sentence
-                markov.create_states(corpus)
-                random_walk_over = markov.construct_sentence()
-                output = random_walk_over[0].upper() + random_walk_over[1:]
 
-                print("OUTPUT SENTENCE: {}...".format(output[:140]))
+
+            def initialize_chain(self):
+                n = self.order
+                chain = []
+
+                #tuple with N size
+                for _ in range(n):
+                    chain.append("")
+
+                return tuple(chain)
+
+
+
+
+            def add_to_chain(self, old, step):
+                new = old[1:] + (step, )
+                return new
+
+
+
+
+            def create_states(self, tokens):
+                """Creating states from the sentences"""
+                prev = self.initialize_chain()
+
+                #Create Markov
+                for token in tokens:
+                    if not prev in self.states:
+                        self.states[prev] = []
+
+                        cur = self.add_to_chain(prev, token)
+                        self.states[prev].append(cur)
+
+                        prev = cur
+                input_sentence = " ".join(tokens)
+                print("\nInput Sentece: {}... \n".format(input_sentence[:400]))
+                print("WORD COUNT OF CORPUS: >400,000\n")
+
                 return
-            
+
+
+
+
+            def construct_sentence(self):
+                """Sample Dictagram and build new sentences"""
+
+            chain = self.initialize_chain()
+            cur_position = self.select_cur_position(chain)
+            start, delimiter, is_first = "", " ", True
+
+            print("CHAIN: {}\n".format(chain))
+
+            while (cur_position in self.states) and (cur_position != chain) and (cur_position != None):
+                if not is_first:
+                    start += delimiter
+
+                    start += cur_position[len(cur_position) -1]
+                    cur_position = self.select_cur_position(cur_position)
+                    is_first = False
+
+            walk_over = start + delimiter + cur_position[len(cur_position) -1]
+
+            return walk_over
+
+
+
+
+            def select_cur_position(self, pos):
+                """Method to select word position in the corpus randomly"""
+                cur_position = self.states[pos][ri(0, len(self.state[pos]) -1)]
+                return cur_position
+
+
+
+
+            def create_markov_model():
+                """Create and run class instance, create copus from Harry Potter book"""
+
+                with open("harry_potter_books.txt") as f:
+                    corpus = f.read().split()
+
+                    if len(sys.argv) > 1:
+                        if sys.argv[1].isnumeric():
+                            print("Numeric input received successfully... LODING... \n\nORDER OF MARKOV:\nn = {}".format(int(sys.argv[1])))
+                            markov = Markov_chain_Nth_order(int(sys.argv[1]))
+
+                        else:
+                            print("ERROR INPUT INVALID. REVERTING TO DEFAULT:\n\nORDER OF MARKOV:\n\n = 1")
+                            markov = Markov_chain_Nth_order()
+
+                    else:
+                        print("DID NOT RECEIVED AN INPUT... REVERTING TO DEFAULT:\n\nORDER OF MARKOV:\nn =1")
+                        markov = Markov_chain_Nth_order()
+
+                        # Random walk over then reformating to final sentence
+                        markov.create_states(corpus)
+                        random_walk_over = markov.construct_sentence()
+                        output = random_walk_over[0].upper() + random_walk_over[1:]
+
+                        print("OUTPUT SENTENCE: {}...".format(output[:140]))
+                        return
+
+
+
+
+            if __name__ == '__main__':
+                main()
