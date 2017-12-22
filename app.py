46c3c9f1a168291d617f0d97ce4f029c99b1cb57
Briant Oliveira <briantmoliveira@gmail.com>
2017-12-22 01:44:19 -0500

    clean test

---
+++ app.py
@@ -0,0 +1,16 @@
+"""
+Import dependencies
+"""
+from flask import flask
+import cleanup as c
+import os
+
+app = Flask(__name__)
+
+"""
+ROUTES
+"""
+def random_sentence():
+    clean_list = s.clean_txt)
+    clean_list.append("STOP")
+    sentence = s.c
---
+++ clean.py
@@ -0,0 +1,30 @@
+from string import punctuation
+
+class Clean():
+
+    def __init__(self):
+        pass
+
+    def _erase_pontuation(self, file_name):
+        with open(file_name, 'r') as myfile:
+            dialouge_list = myfile.read()
+        long_string = ''.join(dialouge_list)
+        long_string = ''.join(c for c in long_string if c not in punctuation)
+        return long_string
+
+    def single_words(self, long_string):
+        word_list = long_string.lower()
+        word_list = word_list.split()
+        return word_list
+
+    def text_cleaner(self, file_name):
+        long_string = self._erase_pontuation(file_name)
+        word_list = self.single_words(long_string)
+        return word_list
+
+def main():
+    file_name = 'harry_potter_books.txt'
+    Clean().clean_txt(file_name)
+
+if __name__ == '__main__':
+    main()
